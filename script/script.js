92e8f9d70118c4161fa0ecaf606c0a11c14b9744
Brendan Tyler <bkrendan12@gmail.com>
2021-03-22 17:33:07 -0400

    added if statements for the password length prompt limiting the user to only use 8 and 128 characters in their password

--- script/script.js
+++ script/script.js
@@ -1,26 +1,38 @@
 
-// Assignment Code
+// // Arrays
 let generateBtn = document.querySelector("#generate");
+
 let lowercaseLettersArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 let uppercaseLettersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 let numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 let specialCharactersArray = ["!", "@", "#", "%", "^", "&", "*", "(", ")"]
 
-// Write password to the #password input
+// // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
+};
 
+function generatePassword() {
+  let passwordLength = parseInt(prompt("How long do you want your password?"));
+ 
+// prompt user for user password length between 8-128 characters
+// see if you can combine both of these if statements in one
+ if (isNaN(passwordLength)) {
+    alert("You need to use numbers!");
+    return "";
+     }
+ if(passwordLength < 8 || passwordLength > 128) {
+     alert("password must be more than 8 and less than 128");
+     return "";
+ }
+    };
+    
+    
+generateBtn.addEventListener("click", writePassword);{
+    generateBtn.style.backgroundColor = "chocolate";
 }
-
-function generatePassword () {
-  // prompt user for user password length between 8-128 characters
-
-  
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-  
+        
+    
